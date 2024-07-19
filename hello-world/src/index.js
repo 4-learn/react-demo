ba5539613c8dcd4efd1741f97245d698f42f6df4
yillkid <yillkid@gmail.com>
2024-07-19 23:38:36 +0800

    jsx-element

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -4,8 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const testFunction =()=> {
+  return( <button>OK</button> );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello world!</h1>);
+root.render(testFunction());
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
