191d5e756f9451c2164edb4ae7126177ec8d02f5
yillkid <yillkid@gmail.com>
2024-07-19 23:52:29 +0800

    jsx-calculate

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const testFunction =()=> {
-  return( <button>OK</button> );
+const multiButton=()=>{
+  var output=[];
+  for(let i=0;i<4;++i)
+      output.push(<button>我是第{i}個按鍵</button>)
+  return output;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(testFunction());
+root.render(multiButton());
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
