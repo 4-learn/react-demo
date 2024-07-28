140a10d5aa0602478a07f00d7257499152b16c03
yillkid <yillkid@gmail.com>
2024-07-28 15:11:56 +0800

    useContext

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,45 +1,52 @@
+import React, { useState, useContext, createContext } from "react";
 import ReactDOM from "react-dom/client";
-import { useState } from "react";
+
+// 創建一個 UserContext
+const UserContext = createContext();
 
 function Component1() {
   const [user, setUser] = useState("Jesse Hall");
 
   return (
-    <>
+    // 使用 UserContext.Provider 提供 user 狀態給所有子組件
+    <UserContext.Provider value={user}>
       <h1>{`Hello ${user}!`}</h1>
-      <Component2 user={user} />
-    </>
+      <Component2 />
+    </UserContext.Provider>
   );
 }
 
-function Component2({ user }) {
+function Component2() {
   return (
     <>
       <h1>Component 2</h1>
-      <Component3 user={user} />
+      <Component3 />
     </>
   );
 }
 
-function Component3({ user }) {
+function Component3() {
   return (
     <>
       <h1>Component 3</h1>
-      <Component4 user={user} />
+      <Component4 />
     </>
   );
 }
 
-function Component4({ user }) {
+function Component4() {
   return (
     <>
       <h1>Component 4</h1>
-      <Component5 user={user} />
+      <Component5 />
     </>
   );
 }
 
-function Component5({ user }) {
+function Component5() {
+  // 使用 useContext 消費 UserContext
+  const user = useContext(UserContext);
+
   return (
     <>
       <h1>Component 5</h1>
@@ -48,5 +55,5 @@ function Component5({ user }) {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Component1 />);
