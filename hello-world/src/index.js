9e98ee7220195a492c4ec009a4fc116a45b9c008
yillkid <yillkid@gmail.com>
2024-08-04 17:52:20 +0800

    uncontrolled-component

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class UncontrolledForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+
+  handleChange = () => {
+    console.log('Current input value: ' + this.inputRef.current.value);
+  };
+
+  handleSubmit = (event) => {
+    alert('Submitted value: ' + this.inputRef.current.value);
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          ref={this.inputRef}
+          onChange={this.handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<UncontrolledForm />);
