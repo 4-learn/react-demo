9652c150fe3048439c50322ba8214c61c7bdfd84
yillkid <yillkid@gmail.com>
2024-08-06 22:57:19 +0800

    class-components-prob

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 // 定義一個簡單的類別元件
 class Greeting extends Component {
   render() {
+    // 通過 this.props 訪問傳遞給組件的 props
+    const { name } = this.props;
+
     return (
       <div>
-        <h1>Hello, World!</h1>
+        <h1>Hello, {name}!</h1>
         <p>Welcome to my React app.</p>
       </div>
     );
--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -4,4 +4,4 @@ import './index.css';
 import Greeting from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Greeting />);
+root.render(<Greeting name="John" />);
