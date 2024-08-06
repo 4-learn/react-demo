1205c91385c041da0b7a5c2dc353849bef9e754c
yillkid <yillkid@gmail.com>
2024-08-06 22:24:07 +0800

    class-component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// 定義一個簡單的函式組件
-function Greeting() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-      <p>Welcome to my React app.</p>
-    </div>
-  );
+// 定義一個簡單的類別元件
+class Greeting extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <p>Welcome to my React app.</p>
+      </div>
+    );
+  }
 }
 
 export default Greeting;
